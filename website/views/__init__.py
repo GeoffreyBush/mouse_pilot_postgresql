38f2e47fdcc61de2f84cd8189cda9bced04a1b5f
Geoffrey Bush <114226907+GeoffreyBush@users.noreply.github.com>
2024-05-19 23:45:16 +0100

    Add Ruff exceptions to views

--- website/views/__init__.py
+++ website/views/__init__.py
@@ -1,6 +1,6 @@
-from .breeding_wing_views import breeding_wing_dashboard, breeding_wing_view_strain, breeding_wing_add_litter, breeding_wing_view_cage, add_cage, edit_cage
-from .family_tree_views import family_tree, create_family_tree_data
-from .login_views import SignUpView, signup
-from .mice_crud_views import edit_mouse, add_mouse, delete_mouse, edit_history
-from .researcher_views import researcher_dashboard, show_project, show_comment
-from .mice_request_views import show_requests, add_request, confirm_request, show_message
+from .breeding_wing_views import breeding_wing_dashboard, breeding_wing_view_strain, breeding_wing_add_litter, breeding_wing_view_cage, add_cage, edit_cage # noqa F401
+from .family_tree_views import family_tree, create_family_tree_data # noqa F401
+from .login_views import SignUpView, signup # noqa F401
+from .mice_crud_views import edit_mouse, add_mouse, delete_mouse, edit_history # noqa F401
+from .researcher_views import researcher_dashboard, show_project, show_comment # noqa F401
+from .mice_request_views import show_requests, add_request, confirm_request, show_message # noqa F401
