a1010e415ff09826fb12473eb15e6f86c5174911
Geoffrey Bush <114226907+GeoffreyBush@users.noreply.github.com>
2024-05-24 15:36:05 +0100

    Remoev duplicate form for add mouse to repo

--- website/forms.py
+++ website/forms.py
@@ -7,6 +7,8 @@ from .models import BreedingCage, Comment, CustomUser, Mouse, Project, Request, 
 
 
 class ProjectMiceForm(forms.ModelForm):
+
+    # Export SEX_CHOICES and TRUE_OR_FALSE to constants.py
     SEX_CHOICES = [
         ("M", "Male"),
         ("F", "Female"),
@@ -77,7 +79,10 @@ class RepositoryMiceForm(forms.ModelForm):
         ("M", "Male"),
         ("F", "Female"),
     ]
-
+    TRUE_OR_FALSE = [
+        (False, "No"),
+        (True, "Yes"),
+    ]
 
     sex = forms.ChoiceField(
         choices=SEX_CHOICES, widget=forms.Select(attrs={"class": "form-select"})
@@ -133,45 +138,6 @@ class RepositoryMiceForm(forms.ModelForm):
     class Meta:
         model = Mouse
         fields = "__all__"  # or list the fields you want to include
-
-
-class RepositoryMiceForm(forms.ModelForm):
-    SEX_CHOICES = [
-        ("M", "Male"),
-        ("F", "Female"),
-    ]
-    TRUE_OR_FALSE = [
-        (False, "No"),
-        (True, "Yes"),
-    ]
-
-    sex = forms.ChoiceField(
-        choices=SEX_CHOICES, widget=forms.Select(attrs={"class": "form-select"})
-    )
-    dob = forms.DateField(
-        input_formats=["%Y-%m-%d"],
-        widget=forms.DateInput(format="%Y-%m-%d", attrs={"type": "date", "class": "form-control"}),
-    )
-    clippedDate = forms.DateField(
-        required=False,
-        input_formats=["%Y-%m-%d"],
-        widget=forms.DateInput(format="%Y-%m-%d", attrs={"type": "date", "class": "form-control"}),
-    )
-    mother = forms.ModelChoiceField(queryset=Mouse.objects.all(), required=False, widget=forms.Select(attrs={"class": "form-select"}))
-    father = forms.ModelChoiceField(queryset=Mouse.objects.all(), required=False, widget=forms.Select(attrs={"class": "form-select"}))
-    project = forms.ModelChoiceField(queryset=Project.objects.all(), required=False, widget=forms.Select(attrs={"class": "form-select"}))
-    genotyped = forms.ChoiceField(
-        required=True, choices=TRUE_OR_FALSE, widget=forms.Select(attrs={"class": "form-select"})
-    )
-    earmark = forms.ChoiceField(choices=EARMARK_CHOICES_PAIRED, widget=forms.Select(attrs={"class": "form-select"}))
-    genotyper = forms.ModelChoiceField(
-        queryset=CustomUser.objects.all(), required=False, widget=forms.Select(attrs={"class": "form-select"})
-    )
-    strain = forms.ModelChoiceField(queryset=Strain.objects.all(), required=False, widget=forms.Select(attrs={"class": "form-select"}))
-
-    class Meta:
-        model = Mouse
-        fields = "__all__" 
 
 class CommentForm(forms.ModelForm):
 
