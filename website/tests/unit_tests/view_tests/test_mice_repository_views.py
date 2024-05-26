9101cc2274c9bd6014ae4fc56a1190ef451978bd
GeoffreyBush <GeoffreyBush@users.noreply.github.com>
2024-05-26 19:47:55 +0000

    Automated code formatting applied.

--- website/tests/unit_tests/view_tests/test_mice_repository_views.py
+++ website/tests/unit_tests/view_tests/test_mice_repository_views.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 from django.urls import reverse
 
-from website.tests.factories import UserFactory, MouseFactory
+from website.tests.factories import MouseFactory, UserFactory
 
 
 class MiceRepositoryViewTest(TestCase):
