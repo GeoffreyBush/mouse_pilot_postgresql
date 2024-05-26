2129fc9a7dedfb28e48e88fa3c1bf5b878cbf285
Geoffrey Bush <114226907+GeoffreyBush@users.noreply.github.com>
2024-05-26 20:43:46 +0100

    Finish splitting view tests

--- website/tests/unit_tests/view_tests/test_login_views.py
+++ website/tests/unit_tests/view_tests/test_login_views.py
@@ -1,20 +1,10 @@
-
 from django.test import TestCase
 from django.urls import reverse, reverse_lazy
 
 from website.forms import CustomUserCreationForm
 from website.models import CustomUser
-from website.tests.factories import UserFactory
 from website.views import SignUpView
 
-#######################
-### CONFIRM REQUEST ###
-#######################
-
-
-######################
-### CREATE ACCOUNT ###
-######################
 class SignUpViewTest(TestCase):
 
     # GET CustomUseCreationrForm
@@ -57,32 +47,3 @@ class SignUpViewTest(TestCase):
         self.assertEqual(SignUpView.form_class, CustomUserCreationForm)
         self.assertEqual(SignUpView.success_url, reverse_lazy("login"))
         self.assertEqual(SignUpView.template_name, "registration/signup.html")
-
-
-#######################
-### MICE REPOSITORY ###
-#######################
-
-
-class MiceRepositoryViewTest(TestCase):
-    def setUp(self):
-        self.user = UserFactory()
-        self.client.login(username="testuser", password="testpassword")
-
-    # GET mice_repository while logged in
-    def test_mice_repository_view_get_request(self):
-        response = self.client.get(reverse("mice_repository"))
-        self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, "repository/mice_repository.html")
-        self.assertIn("mymice", response.context)
-
-    # test for add mouse to repository
-    def test_add_mouse_to_repository(self):
-        response = self.client.get(reverse("add_mouse_to_repository"))
-        self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, "repository/add_mouse_to_repository.html")
-        # self.assertIsInstance(response.context["form"], RepositoryMiceForm)
-
-    # POST RequestForm with valid data
-    # def test_add_mouse_to_repository_post_valid(self):
-    #   data = {
---
+++ website/tests/unit_tests/view_tests/test_mice_repository_views.py
@@ -0,0 +1,29 @@
+
+from django.test import TestCase
+from django.urls import reverse
+
+from website.tests.factories import UserFactory
+
+
+class MiceRepositoryViewTest(TestCase):
+    def setUp(self):
+        self.user = UserFactory(username="testuser")
+        self.client.login(username="testuser", password="testpassword")
+
+    # GET mice_repository while logged in
+    def test_mice_repository_view_get_request(self):
+        response = self.client.get(reverse("mice_repository"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "repository/mice_repository.html")
+        self.assertIn("mymice", response.context)
+
+    # test for add mouse to repository
+    def test_add_mouse_to_repository(self):
+        response = self.client.get(reverse("add_mouse_to_repository"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "repository/add_mouse_to_repository.html")
+        # self.assertIsInstance(response.context["form"], RepositoryMiceForm)
+
+    # POST RequestForm with valid data
+    # def test_add_mouse_to_repository_post_valid(self):
+    #   data = {
