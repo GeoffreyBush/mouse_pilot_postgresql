af6d928bd0bb13b32636cd80ce756b188a65a4d5
GeoffreyBush <GeoffreyBush@users.noreply.github.com>
2024-06-08 22:07:40 +0000

    Automated code formatting applied.

--- mice_requests/models.py
+++ mice_requests/models.py
@@ -1,5 +1,6 @@
-from django.db import models
 from django.core.exceptions import ValidationError
+from django.db import models
+
 from system_users.models import CustomUser
 
 
--- mice_requests/tests.py
+++ mice_requests/tests.py
@@ -1,7 +1,7 @@
+from django.core.exceptions import ValidationError
 from django.db.utils import IntegrityError
 from django.test import TestCase
 from django.urls import reverse
-from django.core.exceptions import ValidationError
 
 from mice_requests.forms import RequestForm
 from mice_requests.models import Request
