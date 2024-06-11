f87000d5a45411e29bc98c4bfec5955efdc8779a
Geoffrey Bush <114226907+GeoffreyBush@users.noreply.github.com>
2024-06-11 22:49:20 +0100

    Test multiple error messages for mice that are already clipped or culled

--- mice_requests/tests/test_form.py
+++ mice_requests/tests/test_form.py
@@ -16,20 +16,22 @@ class RequestFormTestCase(TestCase):
         self.assertFalse(self.form.is_valid())
 
     def test_mice_already_culled_in_cull_request(self):
-        self.culled_mouse = MouseFactory(culled=True)
+        self.culled_mouse1 = MouseFactory(culled=True)
+        self.culled_mouse2 = MouseFactory(culled=True)
         self.form = RequestFormFactory.create(
             task_type="Cull",
-            mice=[self.culled_mouse, MouseFactory(culled=False)],
+            mice=[self.culled_mouse1, self.culled_mouse2],
         )
-        self.assertEqual(self.form.errors["mice"], [f"Mouse {self.culled_mouse} has already been culled."])
+        self.assertEqual(self.form.errors["mice"], [f"Mouse {self.culled_mouse1} has already been culled.", f"Mouse {self.culled_mouse2} has already been culled."])
 
     def test_mice_already_clipped_in_clip_request(self):
-        self.clipped_mouse = MouseFactory(earmark="TL")
+        self.clipped_mouse1 = MouseFactory(earmark="TL")
+        self.clipped_mouse2 = MouseFactory(earmark="TR")
         self.form = RequestFormFactory.create(
             task_type="Clip",
-            mice=[self.clipped_mouse, MouseFactory(earmark="")],
+            mice=[self.clipped_mouse1, self.clipped_mouse2],
         )
-        self.assertEqual(self.form.errors["mice"], [f"Mouse {self.clipped_mouse} has already been clipped."])
+        self.assertEqual(self.form.errors["mice"], [f"Mouse {self.clipped_mouse1} has already been clipped.", f"Mouse {self.clipped_mouse2} has already been clipped."])
 
     def test_mice_field_hidden(self):
         self.form = RequestFormFactory.create()
