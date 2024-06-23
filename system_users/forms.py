31cfb212e778cffa8b80819fb5a358e7ce42f75e
Geoffrey Bush <114226907+GeoffreyBush@users.noreply.github.com>
2024-06-23 10:08:40 +0100

    Style create account page

--- system_users/forms.py
+++ system_users/forms.py
@@ -7,26 +7,26 @@ from system_users.models import CustomUser
 class CustomUserCreationForm(UserCreationForm):
 
     username = forms.CharField(
-        widget=forms.TextInput(attrs={"placeholder": "Username"}),
+        widget=forms.TextInput(attrs={}),
         label="Username",
         required=True,
         min_length=5,
         max_length=20,
     )
     email = forms.EmailField(
-        widget=forms.EmailInput(attrs={"placeholder": "Email"}), label="Email"
+        widget=forms.EmailInput(attrs={}), label="Email"
     )
     password1 = forms.CharField(
         label="Password",
         min_length=8,
         max_length=20,
-        widget=forms.PasswordInput(attrs={"placeholder": "Password"}),
+        widget=forms.PasswordInput(attrs={}),
     )
     password2 = forms.CharField(
         label="Confirm Password",
         min_length=8,
         max_length=20,
-        widget=forms.PasswordInput(attrs={"placeholder": "Confirm Password"}),
+        widget=forms.PasswordInput(attrs={}),
     )
 
     class Meta:
