22d9d0f493f5677f5cf0f8ec5b94cf93b91f5c36
Geoffrey Bush <114226907+GeoffreyBush@users.noreply.github.com>
2024-08-24 10:57:25 +0100

    Remove comments

--- main/filters.py
+++ main/filters.py
@@ -6,10 +6,10 @@ from django import forms
 from main.constants import EARMARK_CHOICES_PAIRED, SEX_CHOICES
 from mice_repository.models import Mouse
 
-
 class MouseFilter(django_filters.FilterSet):
 
     earmarks = EARMARK_CHOICES_PAIRED[1:]
+    
 
     sex = django_filters.ChoiceFilter(
         choices=SEX_CHOICES,
@@ -17,6 +17,7 @@ class MouseFilter(django_filters.FilterSet):
         label="Sex:",
         initial="",
     )
+
     earmark = django_filters.ChoiceFilter(
         choices=earmarks,
         widget=forms.Select(attrs={"class": "form-select w-25 ml-3"}),
@@ -57,4 +58,4 @@ class MouseFilter(django_filters.FilterSet):
 
     class Meta:
         model = Mouse
-        fields = ["sex", "earmark"]
+        fields = ["sex", "earmark", "strain", "min_age", "max_age"]
