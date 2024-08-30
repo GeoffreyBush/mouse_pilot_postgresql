34d12746dd3bf85bb9bb317cf4df48a542021fc2
GeoffreyBush <GeoffreyBush@users.noreply.github.com>
2024-08-30 21:11:13 +0000

    Automated code formatting applied.

--- common/templatetags/custom_filters.py
+++ common/templatetags/custom_filters.py
@@ -3,6 +3,7 @@ from django import template
 
 register = template.Library()
 
-@register.filter(name='range')
+
+@register.filter(name="range")
 def filter_range(start, end):
     return range(start, end)
